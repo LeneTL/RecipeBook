eb27dae4d7410d0ade441b4ce3e2bc5dd446c89f
Lene <Lene-tl@hotmail.com>
2025-10-31 15:01:39 +0100

    index.html

---
+++ model.js
@@ -0,0 +1,86 @@
+const model = {
+    app: {
+        main: document.getElementById('main'),
+        currentPage:'startpage',
+        viewerPages: ['homepage', 'startpage', 'login',  'savedRecipe', ],
+        onlyAdminPages: ['recipeInput', 'recipeList', 'editRecipe'],
+        currentUser: null,
+
+    },
+
+    viewstate:{
+        logIn: {
+            name: '',
+            password: '',
+        },
+
+        registerRecipes: {
+            RecipeID: null,
+            name: '',
+            img: '',
+            madeFor: '',
+            ingredients: '',
+            amount: '',
+            howTo: '',
+        },
+        selectedRecipe: null,
+        editRecipe:{
+            name: '',
+            img: '',
+            madeFor: '',
+            ingredients: '',
+            amount: '',
+            howTo: '',
+        },
+        
+
+    },
+
+    data: {
+        registeredUsers: [
+            {userID:1, name: 'Anne', password: 'Mamma', favorites: [1,2]},
+            {userID:2, name: 'Lene', password: 'Admin'},
+
+        ],
+        registeredRecipes: [
+            {   RecipeID: 1,
+                name: "Pannekaker",
+                Img: "",
+                madeFor: "6personer",
+                ingredients: "", //array??????
+                amount: "", //array????
+                howTo: "Ha mel og salt i en bolle; Tilsett halvparten av melken; rør ut klumper og tilsett resten an melken; ha dermed i et og et egg; La røren hvile i 30 minutter før du steker i smør på middels varme", //ARRAY? sfdhjbhjgb
+                favorite: true,
+            }
+        ]
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
