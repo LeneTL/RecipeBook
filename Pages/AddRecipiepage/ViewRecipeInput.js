f7f28ae7f8d57779d8589abceec5833ad26ea054
Lene <Lene-tl@hotmail.com>
2025-11-25 12:42:48 +0100

    x

--- Pages/AddRecipiepage/ViewRecipeInput.js
+++ Pages/AddRecipiepage/ViewRecipeInput.js
@@ -3,7 +3,22 @@ function RecipeInput(){
     return /*HTML*/`
     This is recipeInput
     <button onclick="goTo('homepage')">Back</button>
-
+    <br>
+    <input><input type="Number">
 
     `
+}
+
+function drawInputs(){
+    let html = '';
+    let inputNumber = 2;
+
+    //if input is not empty, add another input ????
+    //forloop for making the inputs off the inputNumber??
+    for(i= 0; i < inputNumber; i++){
+        h
+    };
+
+
+    return html;
 }
